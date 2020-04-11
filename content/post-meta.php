09cd03c192d501b4a3be06ca24a6184480232ec1
Abhisek Pandey <dreamworkers@gmail.com>
2020-04-12 00:49:58 +0200

    Ported WP theme from BB

---
+++ content/post-meta.php
@@ -0,0 +1,24 @@
+<?php
+$date   = date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'r' ) ) );
+$author = get_the_author();
+
+$postMeta = __('Published on', 'founder' ) . ' <span class="post-meta__date">' . $date . '</span> ';
+//$postMeta .= ' ' . __('by', 'founder' ) . ' ' . $author;
+
+$categories = get_the_category( $post->ID );
+
+if ( $categories ) {
+	$postMeta .= __( "in", "founder" ) . ' ';
+    $categoryCount = 1;
+	foreach ( $categories as $category ) {
+        if ($categoryCount > 1) {
+            $postMeta .= '<span class="post-meta__category-separator"></span>';
+        }
+		$postMeta .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" title="' . esc_attr( sprintf( __( 'View all posts in %s', 'founder' ), $category->name ) ) . '" class="post-meta__category-link">' . esc_html( $category->cat_name ) . '</a>';
+        $categoryCount = $categoryCount + 1;
+	}
+}
+?>
+<div class="post-meta">
+	<p><?php echo $postMeta; ?></p>
+</div>
---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php do_action( 'main_bottom' ); ?>
+</section><!-- .main -->
+
+<footer id="site-footer" class="site-footer" role="contentinfo">
+	<?php do_action( 'footer_top' ); ?>
+	<span class="site-footer__copyright">
+       Copyright © <a href="http://bayard.dreamworkers.in">Abhisek Pandey</a>. Powered by <a href="http://www.dreamworkers.in">Dreamworkers Productions</a>. Running on <a href="http://www.wordpress.com">Wordpress</a>.
+    </span>
+</footer>
+</div>
+</div><!-- .overflow-container -->
+
+<?php do_action( 'body_bottom' ); ?>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
